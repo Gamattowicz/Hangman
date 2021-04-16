bcf45b59b5c84efc774d3f47afbd944050b5612d
Gamattowicz <p.romanczuk31@gmail.com>
2021-04-16 21:02:25 +0200

    add text to the buttons

--- main.py
+++ main.py
@@ -5,10 +5,14 @@ import os
 WIDTH, HEIGHT = 900, 600
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('HANGMAN GAME')
+pygame.init()
 
 # Colors
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
+
+# Fonts
+LETTER_FONT = pygame.font.SysFont('arial', 40)
 
 # Button variables
 RADIUS = 30
@@ -18,26 +22,29 @@ letters = []
 # First row coordinates
 startx = round((WIDTH - (RADIUS * 2 + GAP) * 10) / 2)
 starty = 375
+qwerty_keyboard = 'qwertyuiop'
 for i in range(10):
     x = startx + GAP * 2 + (RADIUS * 2 + GAP) * (i % 10)
     y = starty + ((i // 10) * (GAP + RADIUS * 2))
-    letters.append([x, y])
+    letters.append([x, y, qwerty_keyboard[i]])
 
 # Second row coordinates
 startx2 = round((WIDTH - (RADIUS * 2 + GAP) * 9) / 2)
 starty2 = starty + (RADIUS * 2 + GAP)
+qwerty_keyboard2 = 'asdfghjkl'
 for i in range(9):
     x = startx2 + GAP * 2 + (RADIUS * 2 + GAP) * (i % 9)
     y = starty2 + ((i // 9) * (GAP + RADIUS * 2))
-    letters.append([x, y])
+    letters.append([x, y, qwerty_keyboard2[i]])
 
 # Third row coordinates
 startx3 = round((WIDTH - (RADIUS * 2 + GAP) * 7) / 2)
 starty3 = starty + 2 * (RADIUS * 2 + GAP)
+qwerty_keyboard3 = 'zxcvbnm'
 for i in range(7):
     x = startx3 + GAP * 2 + (RADIUS * 2 + GAP) * (i % 7)
     y = starty3 + ((i // 7) * (GAP + RADIUS * 2))
-    letters.append([x, y])
+    letters.append([x, y, qwerty_keyboard3[i]])
 
 # Load assets
 HANGMAN_IMAGES = [pygame.image.load(os.path.join('Assets', f'hangman'
@@ -53,8 +60,10 @@ def draw():
 
     # draw buttons
     for letter in letters:
-        x, y = letter
+        x, y, ltr = letter
         pygame.draw.circle(WIN, BLACK, (x, y), RADIUS, 3)
+        text = LETTER_FONT.render(ltr, 1, BLACK)
+        WIN.blit(text, (x - text.get_width() / 2, y - text.get_height() / 2))
 
     WIN.blit(HANGMAN_IMAGES[mistakes], (150, 50))
     pygame.display.update()
