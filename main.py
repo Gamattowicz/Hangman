6012b5ff006869e55c10a96060eec0f75321a09d
Gamattowicz <p.romanczuk31@gmail.com>
2021-04-17 17:02:13 +0200

    add disappearance of clicked buttons

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ qwerty_keyboard = 'qwertyuiop'
 for i in range(10):
     x = startx + GAP * 2 + (RADIUS * 2 + GAP) * (i % 10)
     y = starty + ((i // 10) * (GAP + RADIUS * 2))
-    letters.append([x, y, qwerty_keyboard[i]])
+    letters.append([x, y, qwerty_keyboard[i], True])
 
 # Second row coordinates
 startx2 = round((WIDTH - (RADIUS * 2 + GAP) * 9) / 2)
@@ -36,7 +36,7 @@ qwerty_keyboard2 = 'asdfghjkl'
 for i in range(9):
     x = startx2 + GAP * 2 + (RADIUS * 2 + GAP) * (i % 9)
     y = starty2 + ((i // 9) * (GAP + RADIUS * 2))
-    letters.append([x, y, qwerty_keyboard2[i]])
+    letters.append([x, y, qwerty_keyboard2[i], True])
 
 # Third row coordinates
 startx3 = round((WIDTH - (RADIUS * 2 + GAP) * 7) / 2)
@@ -45,7 +45,7 @@ qwerty_keyboard3 = 'zxcvbnm'
 for i in range(7):
     x = startx3 + GAP * 2 + (RADIUS * 2 + GAP) * (i % 7)
     y = starty3 + ((i // 7) * (GAP + RADIUS * 2))
-    letters.append([x, y, qwerty_keyboard3[i]])
+    letters.append([x, y, qwerty_keyboard3[i], True])
 
 # Load assets
 HANGMAN_IMAGES = [pygame.image.load(os.path.join('Assets', f'hangman'
@@ -61,10 +61,11 @@ def draw():
 
     # draw buttons
     for letter in letters:
-        x, y, ltr = letter
-        pygame.draw.circle(WIN, BLACK, (x, y), RADIUS, 3)
-        text = LETTER_FONT.render(ltr, 1, BLACK)
-        WIN.blit(text, (x - text.get_width() / 2, y - text.get_height() / 2))
+        x, y, ltr, visible = letter
+        if visible:
+            pygame.draw.circle(WIN, BLACK, (x, y), RADIUS, 3)
+            text = LETTER_FONT.render(ltr, 1, BLACK)
+            WIN.blit(text, (x - text.get_width() / 2, y - text.get_height() / 2))
 
     WIN.blit(HANGMAN_IMAGES[mistakes], (150, 50))
     pygame.display.update()
@@ -84,10 +85,11 @@ def main():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 mouse_x, mouse_y = pygame.mouse.get_pos()
                 for letter in letters:
-                    x, y, ltr = letter
-                    dist = sqrt((x - mouse_x)**2 + (y - mouse_y)**2)
-                    if dist < RADIUS:
-                        print(ltr)
+                    x, y, ltr, visible = letter
+                    if visible:
+                        dist = sqrt((x - mouse_x)**2 + (y - mouse_y)**2)
+                        if dist < RADIUS:
+                            letter[3] = False
     pygame.quit()
 
 
